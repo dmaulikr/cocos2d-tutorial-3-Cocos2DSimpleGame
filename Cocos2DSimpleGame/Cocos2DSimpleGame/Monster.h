7a4622073e22814aefaff9a662c093ab8c107d84
HolyenZou <holyenzou@gmail.com>
2013-05-08 17:39:50 +0800

    add monster.h/.m file

---
+++ Cocos2DSimpleGame/Cocos2DSimpleGame/Monster.h
@@ -0,0 +1,16 @@
+//
+//  Monster.h
+//  Cocos2DSimpleGame
+//
+//  Created by Holyen Zou on 13-5-8.
+//  Copyright 2013年 demo.HolyenZou.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Monster : CCSprite {
+    
+}
+
+@end
---
+++ Cocos2DSimpleGame/Cocos2DSimpleGame/Monster.m
@@ -0,0 +1,14 @@
+//
+//  Monster.m
+//  Cocos2DSimpleGame
+//
+//  Created by Holyen Zou on 13-5-8.
+//  Copyright 2013年 demo.HolyenZou.com. All rights reserved.
+//
+
+#import "Monster.h"
+
+
+@implementation Monster
+
+@end
